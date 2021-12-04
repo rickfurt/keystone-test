629968a2deff51092c0e1e0bce60bff667fe27fa
Ricardo <ricardofurtado2609@gmail.com>
2021-12-04 14:48:33 +1100

    chore: initial config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+
+ const { withKeystone } = require("@keystone-6/core/next");
+
+ module.exports = withKeystone({
+    reactStrictMode: true,
+ });
